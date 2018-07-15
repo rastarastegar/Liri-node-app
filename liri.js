150e22b3b725868f8ed1d3f21148de00f28fb1e0
Reza R <rastegar8@gmail.com>
2018-07-14 21:12:21 -0500

    Twitter is working now

--- liri.js
+++ liri.js
@@ -1,15 +1,16 @@
 // Requesting the packages and access to key file
 
-const nev = require ( "dotenv").config();
-const keys = require ( "./keys");
-const fs = require ("fs");
-var Twitter = require('twitter'); // from npm instruction
+const nev = require("dotenv").config();
+const keys = require("./keys");
+const fs = require("fs");
+const Spotify = require('node-spotify-api');
+const  Twitter = require('twitter'); // from npm instruction
 
 
 // getting access to keys for spotify and twitter 
 // const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
-const triologyomdbkey= "triology"
+const triologyomdbkey = "triology"
 
 //Set defaults
 var song = "The Sign by Ace of Base";
@@ -20,34 +21,32 @@ var defaultMovieMessage = "If you haven't watched 'Mr. Nobody,' then you should:
 // ********Now functions*********
 
 //Function that sends request to Twitter from npm instruction
-function twitterRequest(){
+function twitterRequest() {
 
-    var params = {screen_name: 'Reza03583634', count: 20};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+    var params = { screen_name: 'Reza03583634', count: 20 };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function (element) {
+                console.log(element.text);
+            });
+        }
+    });
 }
-twitterRequest(); // Is this neccessary?
-
-
-// // Spotify Request 
-// function spotifyRequest(){
+twitterRequest(); // Is this neccessary? (yes it is)
 
 
 
-// var spotify = new Spotify({
-//     id: <your spotify client id>,
-//     secret: <your spotify client secret>
-//   });
-   
-//   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-   
-//   console.log(data); 
-//   });
+// Spotify Request 
+function spotifyRequest() {
 
-// }
+     var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+}
+spotifyRequest()
